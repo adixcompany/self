d86b2ed9d7201308b777b21bda96726cc7180bba
ADiX Company <arashranjer026@gmail.com>
2017-03-10 21:05:42 +0330

    Update beyond.sh

--- beyond.sh
+++ beyond.sh
@@ -4,6 +4,17 @@ cd $HOME/Self-BotV2
 
 install() {
 	    cd tg
+		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+		sudo apt-get install g++-4.7 -y c++-4.7 -y
+		sudo apt-get update
+		sudo apt-get upgrade
+		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
+		sudo apt-get install screen -y
+		sudo apt-get install tmux -y
+		sudo apt-get install libstdc++6 -y
+		sudo apt-get install lua-lgi -y
+		sudo apt-get install libnotify-dev -y
+		sudo service redis-server restart
 		wget https://valtman.name/files/telegram-cli-1222
 		mv telegram-cli-1222 tgcli
 		chmod +x tgcli
@@ -47,6 +58,23 @@ if [ ! -f ./tg/tgcli ]; then
     echo "tg not found"
     echo "Run $0 install"
     exit 1
+ fi
+
+
+   print_logo
+   echo -e "\033[38;5;208m"
+   echo -e "     >>>>                       We Are Not Attacker                             "
+   echo -e "     >>>>                       We Are Not Alliance                             "
+   echo -e "     >>>>                       We Are Family                                   "
+   echo -e "     >>>>                       We Are The Best :-)                             "
+   echo -e "     >>>>                       @BeyondTeam                                     "
+   echo -e "\033[0;00m"
+   echo -e "\e[36m"
+   logo_play
+   #sudo service redis-server restart
+   #./tg/tgcli -s ./bot/bot.lua -l 1 -E $@
+   ./tg/tgcli -s ./bot/bot.lua $@
+fi
  fi
 
 
